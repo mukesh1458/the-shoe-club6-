1adc496419ae19c1493753fbd163c3e601878c7f
mukesh chowdary <msai81398@gmail.com>
2025-12-21 22:07:12 +0530

    fix: add missing path import

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 
 const app = express();
 
